65c057dc375b09b3d4d6ab0c2c1f0f6d0147d5f1
Youssef_nakhli <youssef1nakhli@gmail.com>
2024-04-25 19:36:01 +0000

    Correction des fichier XML

--- app/src/main/java/com/example/fitnesstracker/view/MainActivity.java
+++ app/src/main/java/com/example/fitnesstracker/view/MainActivity.java
@@ -3,14 +3,25 @@ package com.example.fitnesstracker.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.fitnesstracker.R;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button btnConnexion,btnCreation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private void init(){
+        editTextPassword=findViewById(R.id.editTextPassword);
+        editTextUsername=findViewById(R.id.editTextUsername);
+
+    }
+
 }
